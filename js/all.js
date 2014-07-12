47990599de7d7391d2a209312dd108edde87fd9a
shen2 <zhenyu@duoshuo.com>
2014-07-12 15:30:34 +0800

    first commit

---
+++ js/all.js
@@ -0,0 +1,87 @@
+(function(window, $){
+
+var NAME = function(){
+	this.defaultRoot = 'README.md';
+	this.defaultIndex = 'index.md';
+	
+	this.parser = {};
+	this.parser['md'] = this.parser['markdown'] = function(rsp){
+		this.html(marked(rsp, true));
+		document.title = this.find('h1').text();
+		
+		return this;
+	};
+	
+	this.parser['txt'] = $.fn.text;
+	this.parser['html'] = $.fn.html;
+};
+
+NAME.prototype.navigate = function(pathname){
+	var self = this;
+	var url = pathname;
+	
+	if (pathname === ''){
+		url += this.defaultRoot;
+	}
+	else if (pathname.charAt(pathname.length - 1) === '/'){
+		url += this.defaultIndex;
+	}
+	
+	$.ajax({
+		url  : url,
+	    type: 'GET',
+		complete: function(){},
+		success: function(rsp, result, xhr){
+			var main = $('main');
+			
+			var matches = url.match(/\.([a-z0-9]+)$/i);
+			
+			var parser = matches && self.parser[matches[1]] ? self.parser[matches[1]] : self.parser['html'];
+			
+			parser.call(main, rsp);
+		},
+		error : function(){
+			var main = $('main');
+			main.html('<h1>404 Not Found</h1>');
+		}
+	});
+};
+
+NAME.prototype.autoload = function (list, ext){
+	list.each(function(){
+		var el = $(this);
+		$.ajax({
+			url  : this.id +'.' + ext,
+		    type: 'GET',
+			complete: function(){},
+			success: function(rsp, result, xhr){
+				instance.parser[ext].call(el, rsp);
+			}
+		});
+	});
+};
+
+NAME.prototype.dispatch = function(){
+	var self = this;
+	function router(e){
+		var hash = window.location.hash;
+		if (hash === ''){
+			self.navigate('');
+			return false;
+		}
+		
+		var matches = hash.match(/^#!(.*)/i);
+		if (matches){
+			self.navigate(matches[1]);
+			return false;
+		}
+	}
+	
+	$(window).on('hashchange', router);
+	
+	router();
+};
+
+window.NAME = NAME;
+
+})(window, jQuery);
---
+++ nav.md
@@ -0,0 +1,7 @@
+[多说API文档](#!)
+
+### 评论
+  * [查看评论](#!post/create.md)
+  * [更新图博](#!post/update.md)
+  * [删除图博](#!post/delete.md)
+
